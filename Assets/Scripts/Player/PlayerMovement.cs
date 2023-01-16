5b601249f75a7397d36f8e82f905645de79b9566
Jay Mehta <91770194+jaymehta-g@users.noreply.github.com>
2023-01-16 11:51:55 -0600

    add script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
